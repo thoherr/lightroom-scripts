94ecc05ad83e5f6bf2bfb5a87d743ac66008d9e0
Thomas Herrmann <mail@thoherr.de>
2016-06-17 23:46:26 +0200

    first skeleton (nothing working yet)

---
+++ src/models/ag_library_file.rb
@@ -0,0 +1,3 @@
+class AgLibraryFile < ActiveRecord::Base
+end
+
---
+++ src/scripts/list_library_files.rb
@@ -0,0 +1,9 @@
+# TODO: Better way for paths
+require File.expand_path('../../models/ag_library_file', __FILE__)
+
+class ListLibraryFiles
+  
+  def run
+  end
+  
+end
